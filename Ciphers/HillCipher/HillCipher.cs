08878f69fee7c85a295db05364d6f864a7332719
Ariadne Petroulakis <ariadnepet@icloud.com>
2024-11-02 23:43:38 -0600

    Wrote FormatNumMatrix and added unimplemented helpers for matrix multiplication.

--- Ciphers/HillCipher/HillCipher.cs
+++ Ciphers/HillCipher/HillCipher.cs
@@ -6,13 +6,16 @@ using System.Reflection.Metadata.Ecma335;
 ///<version>November 2024</version>
 namespace HillCipher
 {
-    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     /// <summary>
     /// This class represents or creates a Hill cipher of a given string of text. 
     /// </summary>
     public static class HillCipher
     {
+        /// <summary>
+        /// a variable to hold all the letters in the alphabet with their indexes. 
+        /// </summary>
+        private static char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         /// <summary>
         /// Encrypts a message using the hill cipher. 
         /// </summary>
@@ -21,20 +24,16 @@ namespace HillCipher
         /// <param name="message"> the message to be encrypted. Must contain only letters</param>
         /// <returns> the encrypted message </returns>
         /// <exception cref="ArgumentException"> if the key or message is invalid </exception>
-        public static string Encrypt(int[][] key, string message) 
+        public static string Encrypt(int[,] key, string message)
         {
             //1) check if key and message are valid. if message is not dividible by 2, add X to the end
             if (!(CheckKeyValidity(key, out string? err) && CheckMessageValidity(message, out err)))
                 throw new ArgumentException(err);
 
-            //ensure that the char array is dividible by 2 so the chunks work out. 
-            if (message.Length % 2 != 0)
-                message += "x";
-
-            char[] c = message.ToLower().ToCharArray(); 
 
             //2) set all letters in the string to a 2D array of their position in the alphabet broken into sub-vectors of length n (key.length)
-                // > each column must be of length n 
+            int[,] vectorCollection = FormatNumMatrix(message, key.Length);
+
             //3) multiply the key into each column 
             //4) translate the columns into letters and concatenate together the contents of each row anf THEN each column
             int[] positions = new int[message.Length];
@@ -48,10 +47,10 @@ namespace HillCipher
         /// <param name="key"> the matrix used as a key for the encryption </param>
         /// <param name="err"> the error message to be displayed</param>
         /// <returns> true if the matrix is invertible modulo 26, false otherwise </returns>
-        private static bool CheckKeyValidity(int[][] key, out string? err)
+        private static bool CheckKeyValidity(int[,] key, out string? err)
         {
             //check if its square: a non-square matrix cannot be invertible
-            if (key.Length != key[0].Length)
+            if (key.Length != key.GetLength(0))
             {
                 err = "Key matrix is not square and thus not invertible.";
                 return false;
@@ -68,7 +67,7 @@ namespace HillCipher
             key = Mod26(key);
 
             //a 2x2 invertible matrix has a set formula for when it is and isn't invertible
-            if ((key[0][0] * key[1][1]) - (key[0][1] * key[1][0]) != 0)
+            if ((key[0,0] * key[1,1]) - (key[0,1] * key[1,0]) != 0)
             {
                 err = "This 2x2 matrix as a determinant of 0 and is not invertible";
                 return true;
@@ -103,21 +102,75 @@ namespace HillCipher
         /// </summary>
         /// <param name="arr"> a matrix </param>
         /// <returns> a modified version of the original matrix where each entry becomes itself mod 26. </returns>
-        private static int[][] Mod26(int[][] arr)
+        private static int[,] Mod26(int[,] arr)
         {
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < arr[i].Length; j++)
+                for (int j = 0; j < arr.GetLength(i); j++)
                 {
-                    arr[i][j] = arr[i][j] % 26;
+                    arr[i,j] = arr[i,j] % 26;
                 }
             }
             return arr;
         }
 
         /// <summary>
-        /// Decrypts a message encrypted using the hill cipher. 
+        /// Helper method to set up a matrix for easy multiplication in the next step. Each column is a vector that must
+        /// be multiplied later with the key. There are n rows (where n = m = key matrix length/width) and columns equal 
+        /// to half the number of letters in the message, rounded up to the closest even number.
         /// </summary>
+        /// <param name="message"> the message to turn into an array of coded numbers </param>
+        /// <param name="numRows"> the number of rows/cols in the key. </param>
+        /// <returns> the int array </returns>
+        private static int[,] FormatNumMatrix(string message, int numRows)
+        {
+            if (message.Length % 2 != 0)
+                message += "x";
+
+            char[] toEncode = message.ToLower().ToCharArray();
+
+            int numCols = toEncode.Length / 2;
+            int[,] intArr = new int[numRows, numCols];
+
+            int index = 0; //keeps track of the toEncode index
+            for (int col = 0; col < numCols; col++) //fill out each column, but:
+            {
+                for (int row = 0; row < numRows; row++) //only after you fill out veery row in that column.
+                {
+                    intArr[row, col] = Array.IndexOf(alphabet, toEncode[index]);
+                    index++;
+                }
+            }
+
+            return intArr;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        private static int[,] MultiplyIntoCollection(int[,] key, int[,] collection)
+        {
+            //for each col of the collection 
+            //col = key * col
+            //mod 26
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Multiply a matrix by a vector, given they can be multiplied. 
+        /// </summary>
+        /// <param name="left"> </param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        private static int[,] MultiplyMatrices(int[,] left, int[,] right)
+        {
+            throw new NotImplementedException();
+        }
+
         /// <param name="key">the key used to create the encryption</param>
         /// <param name="message"> the encrypted message</param>
         /// <returns> the decrypted message </returns>
